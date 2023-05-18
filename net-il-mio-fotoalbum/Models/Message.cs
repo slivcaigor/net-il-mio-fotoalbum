f52c5560bcbc0e4f4703c21fc0c08b43d1f771ad
slivcaigor <slivcaigor@gmail.com>
2023-05-18 09:51:14 +0200

    feat: add login and logout functionality to Identity pages
    Add Login and Logout pages to the Identity area of the application. The Login page contains a form for users to enter their email and password to log in, as well as links to register as a new user or reset their password. The Logout page allows users to log out of the application.
    
    The Login and Logout pages are backed by corresponding C# classes that handle the form submissions and log the user in or out of the application.
    
    feat(register): add register page and functionality to allow users to create an account
    feat(register): add validation scripts partial to register page for client-side validation
    feat(register): add view imports and view start files to register page for better organization and consistency
    
    feat(Migrations): add initial migration for PhotoContext with tables for categories, photos, messages, and identity tables for users, roles, and claims.
    
    feat(Migrations): add initial database setup migration with tables for users, roles, categories, photos, and messages. Also, add many-to-many relationship between categories and photos.
    
    chore(PhotoContextModelSnapshot.cs): update PhotoContextModelSnapshot to use the latest version of EF Core
    feat(Program.cs): add support for Identity and configure connection string for PhotoContext
    refactor(Message.cs): add Column attribute to Message properties to improve semantics
    refactor(_Layout.cshtml): remove Privacy link from navbar and add LoginPartial view component
    
    feat(LoginPartial.cshtml): add login and registration links to navbar
    feat(net-il-mio-fotoalbum.csproj): add AWSSDK.S3, Microsoft.AspNetCore.Identity.EntityFrameworkCore, Microsoft.AspNetCore.Identity.UI, Microsoft.EntityFrameworkCore.Design, Microsoft.EntityFrameworkCore.SqlServer, and Microsoft.VisualStudio.Web.CodeGeneration.Design packages to project dependencies

--- net-il-mio-fotoalbum/Models/Message.cs
+++ net-il-mio-fotoalbum/Models/Message.cs
@@ -7,12 +7,16 @@ namespace net_il_mio_fotoalbum.Models
     public class Message
     {
         [Key]
+        [Column("id")]
         public int Id { get; set; }
 
+        [Column("name")]
         public string Name { get; set; }
 
+        [Column("email")]
         public string Email { get; set; }
 
+        [Column("text")]
         public string Text { get; set; }
     }
 }
--- net-il-mio-fotoalbum/Program.cs
+++ net-il-mio-fotoalbum/Program.cs
@@ -1,4 +1,25 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using net_il_mio_fotoalbum;
+
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("PhotoContextConnection") ?? throw new InvalidOperationException("Connection string 'PhotoContextConnection' not found.");
+
+builder.Services.AddDbContext<PhotoContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<PhotoContext>();
+
+builder.Configuration.AddJsonFile("appsettings.json");
+
+builder.Services.AddDbContext<PhotoContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("FotoAlbumDB")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<PhotoContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -18,10 +39,14 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
