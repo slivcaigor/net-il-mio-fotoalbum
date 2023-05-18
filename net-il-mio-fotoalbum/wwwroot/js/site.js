5c5df624197dcd5164f568a928d81a17661c3fe0
slivcaigor <slivcaigor@gmail.com>
2023-05-18 12:21:41 +0200

    Contact form validation + toast messages

--- net-il-mio-fotoalbum/wwwroot/js/site.js
+++ net-il-mio-fotoalbum/wwwroot/js/site.js
@@ -7,6 +7,48 @@
     var email = document.getElementById('email').value;
     var message = document.getElementById('text').value;
 
+    // Validation for name, email and message
+    if (name === "" || email === "" || message === "") {
+        var myToast = Toastify({
+            text: "All fields are required!",
+            className: "errorToast",
+            duration: 5000
+        })
+        myToast.showToast();
+        return;
+    }
+
+    if (name.length > 100) {
+        var myToast = Toastify({
+            text: "Name can't be longer than 100 characters!",
+            background: "linear-gradient(to right, #ff5f6d, #ffc371)",
+            className: "errorToast",
+            duration: 5000
+        })
+        myToast.showToast();
+        return;
+    }
+
+    if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email)) {
+        var myToast = Toastify({
+            text: "Invalid email format!",
+            className: "errorToast",
+            duration: 5000
+        })
+        myToast.showToast();
+        return;
+    }
+
+    if (message.length > 500) {
+        var myToast = Toastify({
+            text: "Message can't be longer than 500 characters!",
+            className: "errorToast",
+            duration: 5000
+        })
+        myToast.showToast();
+        return;
+    }
+
     // Create an object with the data
     var data = {
         name: name,
@@ -19,7 +61,8 @@
             console.log('Data saved successfully: ', response);
 
             var myToast = Toastify({
-                text: "Message sent successfully",
+                text: "Message sent successfully!",
+                className: "successToast",
                 duration: 5000
             })
             myToast.showToast();
@@ -31,7 +74,8 @@
             console.log('An error occurred: ', error);
 
             var myToast = Toastify({
-                text: "An error occurred while sending the message",
+                text: "An error occurred while sending the message.",
+                className: "errorToast",
                 duration: 5000
             })
             myToast.showToast();
